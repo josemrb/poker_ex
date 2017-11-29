29f1821800c1cca1bf9df26c20ca9b0e2ccb8b9f
josemrb <work@josemrb.com>
2017-11-29 15:30:01 -0400

    Add dependencies Credo, ExCoveralls

--- mix.exs
+++ mix.exs
@@ -6,7 +6,9 @@ defmodule Poker.Mixfile do
       app: :poker,
       version: "0.1.0",
       elixir: "~> 1.5",
+      preferred_cli_env: [coveralls: :test],
       start_permanent: Mix.env == :prod,
+      test_coverage: [tool: ExCoveralls],
       deps: deps()
     ]
   end
@@ -21,6 +23,8 @@ defmodule Poker.Mixfile do
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
+      {:credo, "~> 0.8", only: [:dev, :test], runtime: false},
+      {:excoveralls, "~> 0.7", only: :test}
       # {:dep_from_hexpm, "~> 0.3.0"},
       # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"},
     ]
